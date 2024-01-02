14490a66e9ba951d1d15c2fa793fe33612ab048c
siwuagain <01simon.wu@gmail.com>
2024-01-02 15:34:22 -0500

    set up app template + tailwind

---
+++ app/src/components/header/header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
