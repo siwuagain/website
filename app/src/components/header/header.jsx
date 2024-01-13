14f0d9e1bd4d23112213feeb4243e3980ace3c8d
siwuagain <01simon.wu@gmail.com>
2024-01-13 14:08:11 -0500

    linked to pdf

--- app/src/components/header/header.jsx
+++ app/src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import styles from './header.module.css';
 import '../../assets/fonts.css'
+import Resume from '../../assets/resume.pdf'
 const navigation = [
   { name: 'Simon Wu', href: '#SW'},
   { name: 'Projects', href: '#projects'},
@@ -34,13 +35,14 @@ export default function Header() {
         <div>
           <a
             key='Resume'
-            href=''
+            href={Resume}
             className={classNames(
               'resume',
               'text-xl',
               'text-gray-300 hover:bg-gray-700 hover:text-white',
               'rounded-md px-3 py-2 text-sm font-medium'
               )}
+            target='_blank'
             >
             Resume
           </a>
