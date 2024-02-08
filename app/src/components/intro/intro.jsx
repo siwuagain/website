e8539bf90da99e1a73458eedc85768857ae239d3
siwuagain <01simon.wu@gmail.com>
2024-02-07 19:17:33 -0500

    adjusted intro

--- app/src/components/intro/intro.jsx
+++ app/src/components/intro/intro.jsx
@@ -2,7 +2,7 @@ import Animation from "../animation/animation"
 import githubSvg from "../../assets/icons/github.svg"
 import linkedInSvg from "../../assets/icons/linkedin.svg"
 import emailSvg from "../../assets/icons/email.svg"
-import boy from "../../assets/icons/boy.png"
+import boy from "../../assets/animations/boy.gif"
 
 const socials = [
   {href: "https://github.com/siwuagain", src: githubSvg},
@@ -21,16 +21,16 @@ export default function Intro() {
           </a>
           ))}
       </div>
-      <div className="flex flex-row items-center mt-40 justify-between">
-        <div className="text-gray-300 text-xl text-left">
+      <div className="mt-40 flex justify-center">
+        <img className="" src={boy}></img>
+      </div>
+      <div className="flex flex-row items-center mt-20 justify-between">
+        <div className="text-gray-300 text-2xl text-left">
           Hi! I'm Simon, currently studying Computer Science at McGill University. In my free time, 
           I enjoy building projects to discover and learn Web and Software Development. I am most
           experienced with JavaScript/TypeScript, and also well versed with Java, C, and Python. Some of
           my hobbies include the guitar, running, and weightlifting. Let's connect!
 
-        </div>
-        <div >
-          <img className="rounded-lg size-44 shadow-inner" src={boy}></img>
         </div>
       </div>
     </div>
